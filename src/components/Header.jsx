de42b2854402cd25908f951e2541581cb3d0d715
bombay21 <kenechukwu.john@gmail.com>
2021-08-26 12:20:12 +0100

    add axios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import '../styles/Header.css'
+import PersonIcon from "@material-ui/icons/Person"
+import IconButton from "@material-ui/core/IconButton"
 
 function Header() {
     return (
-        <div>
-            
-        </div>
-    )
+      <div className="header">
+        <IconButton>
+          <PersonIcon fontSize="large" className="header__icon" />
+        </IconButton>
+
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
+          alt="logo"
+        />
+      </div>
+    );
 }
 
 export default Header
--- src/services/AxiosGateway.jsx
+++ src/services/AxiosGateway.jsx
@@ -1,2 +1,12 @@
+import React from 'react'
 import axios from 'axios'
 
+function AxiosGateway() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AxiosGateway
